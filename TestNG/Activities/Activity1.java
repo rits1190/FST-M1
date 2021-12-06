8af196f92e2f9d42d63bbc34d11b43d553387d82
rits1190 <84965310+rits1190@users.noreply.github.com>
2021-12-06 17:30:01 +0530

    Add files via upload

---
+++ TestNG/Activities/Activity1.java
@@ -0,0 +1,57 @@
+package testngactivities;
+import java.util.List;
+import java.util.Set;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+public class Activity1 {
+    WebDriver driver;
+    
+    @BeforeMethod
+    public void beforeMethod() {
+        //Create a new instance of the Firefox driver
+        driver = new FirefoxDriver();
+        
+        //Open browser
+        driver.get("https://www.training-support.net");
+    }
+
+    @Test
+    public void exampleTestCase() {
+        // Check the title of the page
+        String title = driver.getTitle();
+            
+        //Print the title of the page
+        System.out.println("Page title is: " + title);
+            
+            //Assertion for page title
+        Assert.assertEquals("Training Support", title);
+                    
+        //Find the clickable link on the page and click it
+        driver.findElement(By.id("about-link")).click();
+                    
+        //Print title of new page
+        System.out.println("New page title is: " + driver.getTitle());
+        
+        Assert.assertEquals(driver.getTitle(), "About Training Support");
+    }
+
+    @AfterMethod
+    public void afterMethod() {
+        //Close the browser
+        driver.quit();
+    }
+
+}
